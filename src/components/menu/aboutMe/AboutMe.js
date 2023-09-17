38172dc43498d7ffb541ba5cbd1f7fdfc13ebef1
jia-son <116625758+jia-son@users.noreply.github.com>
2023-09-17 20:12:09 +0900

    폰트와 애니메이션 적용 close #14

--- src/components/menu/aboutMe/AboutMe.js
+++ src/components/menu/aboutMe/AboutMe.js
@@ -5,12 +5,11 @@ const AboutMe = () => {
     return (
         <>
         <div className="aboutMeBox">
-            <h1>안녕하세요.</h1>
-            <h1>미완성 개발자를 꿈꾸는 손지아입니다.</h1>
+            <div className="aboutMeText">
+                <h1>안녕하세요.</h1>
+                <h1>미완성 개발자를 꿈꾸는 손지아입니다.</h1>
+                <hr />
 
-            <hr />
-            
-            <div>
                 <h3>"미완성 개발자를 목표로"</h3>
 
                 <p>
--- src/components/menu/skills/Skills.js
+++ src/components/menu/skills/Skills.js
@@ -5,14 +5,19 @@ const Skills = () => {
     return (
         <>
         <div className="skillsBox">
-            <h3>SKILLS</h3>
-            <p>
-                개발 언어로 처음 접하게 된 것은 Java였습니다.
-                그 이후로 학원을 다니며 프로젝트에 Spring Boot, React, MySQL, AWS S3를 사용해보았습니다.
-                원활한 협업을 위해서는 Git을 사용했으며, 꾸준히 기능의 수정과 추가를 진행해본 경험이 있습니다.
-                현재 추가로 꾸준히 공부하고 있는 부분으로는 Node.js가 있습니다.
-            </p>
-
+            <div className="skillsFirstText">
+                <h3>SKILLS</h3>
+            </div>
+            
+            <div className="skillsSecondText">
+                <p>
+                    개발 언어로 처음 접하게 된 것은 Java였습니다.
+                    그 이후로 학원을 다니며 프로젝트에 Spring Boot, React, MySQL, AWS S3를 사용해보았습니다.
+                    원활한 협업을 위해서는 Git을 사용했으며, 꾸준히 기능의 수정과 추가를 진행해본 경험이 있습니다.
+                    현재 추가로 꾸준히 공부하고 있는 부분으로는 Node.js가 있습니다.
+                </p>
+            </div>
+            
             <div className="skillsBoxInBox">
                 <div className="skillsLeft">
                     으하하....

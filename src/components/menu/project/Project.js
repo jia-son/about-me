c7b07b7975951a74b38fb6acfd759e3913ce8d76
jia-son <116625758+jia-son@users.noreply.github.com>
2023-09-19 01:21:56 +0900

    프로젝트 이미지와 호버시 띄울 오버레이 카드 추가

--- src/components/menu/project/Project.js
+++ src/components/menu/project/Project.js
@@ -1,16 +1,24 @@
 import React from "react";
 import './project.css';
+import ttImage from '../../img/TT.jpg';
 
-const project = () => {
+const Project = () => {
     return (
-        <>
         <div className="projectBox">
             <div className="projectFirstText">
                 <h3>Project</h3>
             </div>
+            
+            <div className="projectImgBox">
+                <div className="image-container">
+                    <img className="projectImg" src={ttImage} alt="이미지" />
+                    <div className="overlay-card">
+                        <p>이미지 설명 또는 추가 텍스트</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        </>
     );
 }
 
-export default project;
+export default Project;

f7caa019db6337340a9fabe05dc68f635e4b04b8
jia-son <116625758+jia-son@users.noreply.github.com>
2023-09-26 00:49:21 +0900

    메뉴 토글 생성 완료 close #44

--- src/components/leftComponents/LeftConponents.js
+++ src/components/leftComponents/LeftConponents.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './leftComponents.css';
+import { BiChevronsLeft, BiChevronsRight } from "react-icons/bi";
 
 const LeftConponents = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth);
+    const [menu, setMenu] = useState(true);
+
+    const changeMenu = () => {
+        const newMenu = !menu;
+        setMenu(newMenu)
+    }
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth);
+        };
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    if (!menu && isMobile <= 768) {
+        return <BiChevronsRight id="menuRightIcon" onClick={changeMenu} />;
+    }
+
     return (
         <>
         <div className="leftBox">
+            {
+            isMobile <= 768 ? 
+            (
+                <BiChevronsLeft id="menuIcon" onClick={changeMenu} />
+            ) : null
+            }
             <div className="leftBoxInImgBox">
                 <div className="imgBox" />
                 <h1>손지아</h1>
